5cc8364096e36eccda67b0869c49d22ac70080d0
Greg Sheremeta <gshereme@redhat.com>
2017-02-06 12:13:21 -0500

    made yarn.lock file mandatory
    We determined that projects that use ovirt-engine-nodejs-modules
    really need to be locked down with yarn as well. If they're not,
    their builds will often fail as dependencies change.
    
    Therefore, yarn.lock is now mandatory.
    
    Change-Id: Id77b3707c4f194a8ffdd8279e2b582f99eac0616
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

--- automation/build.sh
+++ automation/build.sh
@@ -28,8 +28,8 @@ yarn config set yarn-offline-mirror "${PWD}/${yarn_offline_cache_dir}"
 rm -rf ${yarn_offline_cache_dir}
 
 # The "projects.list" file contains URLs, each one referencing
-# a "package.json" file and the associated "yarn.lock" file (if
-# one exists) for the given project.
+# a "package.json" file and the associated "yarn.lock" file for
+# the given project.
 # When reading the "projects.list" file, make sure to remove
 # blank lines as well as lines starting with the "#" character:
 sed -e '/^[ \t]*$/d' -e '/^#/d' projects.list | while read -r line; do
@@ -42,11 +42,11 @@ sed -e '/^[ \t]*$/d' -e '/^#/d' projects.list | while read -r line; do
     package_json_url="${line/\{FILE\}/package.json}"
     yarn_lock_url="${line/\{FILE\}/yarn.lock}"
 
-    # Download the "package.json" file (required):
+    # Download the "package.json" file:
     wget -O "package.json" "${package_json_url}"
 
-    # Download the "yarn.lock" file (optional):
-    wget -O "yarn.lock" "${yarn_lock_url}" || true
+    # Download the "yarn.lock" file:
+    wget -O "yarn.lock" "${yarn_lock_url}"
 
     # Transform offline entries to online ones. Yarn won't connect for offline entries
     sed -i '/resolved "https:\/\//! s#\([ ]*\)resolved \(.*\)-\([0-9]\+\.[0-9]\+\.[0-9]\+.*\)#\1resolved "https://registry.yarnpkg.com/\2/-/\2-\3"#g' yarn.lock
