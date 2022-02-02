a0592ae8a3ba000c866ef2b7bca3f8ab91e95c3d
Scott Dickerson <sdickers@redhat.com>
2022-02-02 11:49:34 -0500

    Update check action to check across all commits in the PR (#14)
    * Update check action to check across all commits in the PR
    
      - checkout source with depth and with the PR's head commit (instead
        of the default merge commit) so we can `git show` over the range of
        commits in the PR
    
      - `check-version-release.sh` needs to specify a commit range
        to the `git show` command in case a PR has multiple commits
    
      - by default, only look at the head commit of the current branch
    
    * Update spec with new release for the PR
    
    * Remove pre-seed for ovirt-web-ui PR 1557
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- automation/check-version-release.sh
+++ automation/check-version-release.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -e
+#!/bin/bash -ex
 
-if ! git show -- *.spec | \
+if ! git show $1 -- *.spec | \
     grep '^+\(Version:\|Release:\)'
 then
     echo "Package version or release must be updated"
--- automation/check.sh
+++ automation/check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
 # Make sure we remember to update the version and/or release:
-./automation/check-version-release.sh
+if [[ "${1}" == "" ]] ; then
+    ./automation/check-version-release.sh HEAD
+else
+    ./automation/check-version-release.sh $1
+fi
 
 # Make sure we only have 1 instance of yarn
 [[ $(ls -1 yarn-*.js | wc -l) -ne 1 ]] && { echo "Error: multiple yarn binaries present"; exit 5; }
